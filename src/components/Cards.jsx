40ef7ce8ae09045e53e502f10d7c200d28711491
Ziad Rima <ziadrima49@gmail.com>
2025-03-13 13:24:44 +0100

    finish cards component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,16 @@
 import BackCard from "./BackCard"
 import FrontCard from "./FrontCard"
-const Cards = () => {
+const Cards = (props) => {
   return (
     <div className='cards-component'>
       <BackCard />
-      <FrontCard />
+      <FrontCard 
+        cardName={props.cardName}
+        cardNumber={props.cardNumber}
+        month={props.month}
+        year={props.year}
+        valid={props.valid}
+      />
     </div>
   )
 }
