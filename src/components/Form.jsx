2d25ae68df5e9cf4da7fb4f5616b4f557a857c5f
Ziad Rima <ziadrima49@gmail.com>
2025-03-13 13:25:09 +0100

    finish the form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,25 @@
 import Inputs from "./Inputs"
-
-const Form = () => {
+import Button from "./Button"
+const Form = (props) => {
   return (
-    <div className='form-container'>
-      <Inputs />
+    <div className='form-container space-grotesk'>
+      <Inputs 
+        cardName={props.cardName}
+        setCardName={props.setCardName}
+        cardNumber={props.cardNumber}
+        setCardNumber={props.setCardNumber}
+        month={props.month}
+        setMonth={props.setMonth}
+        year={props.year}
+        setYear={props.setYear}
+        cvc={props.cvc}
+        setCvc={props.setCvc}
+        errors={props.errors}
+        setErrors={props.setErrors}
+      />
+      <Button 
+        handleSubmit={props.handleSubmit}
+      />
     </div>
   )
 }
