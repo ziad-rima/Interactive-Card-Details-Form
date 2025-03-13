d85c5309fbbd2e44d1cc899d4b2b40200361f9cb
Ziad Rima <ziadrima49@gmail.com>
2025-03-13 13:24:23 +0100

    finish card name component

--- src/components/CardName.jsx
+++ src/components/CardName.jsx
@@ -1,8 +1,24 @@
-const CardName = () => {
+import { useEffect } from "react"
+
+const CardName = (props) => {
+
+  useEffect(() => {
+    const savedName = localStorage.getItem("name");
+    if (savedName) props.setCardName(savedName);
+  }, [props.setCardName])
+
+  const handleName = (event) => {
+    let value = event.target.value; 
+    props.setCardName(value);
+    localStorage.setItem("name", value);
+  }
+
   return (
     <div className='cardholder-name-container'>
-      <label htmlFor="cardholder-name">CARDHOLDER NAME</label>
-      <input type="text" placeholder="e.g. Jane Appleseed" name="cardholder-name" id="cardholder-name" />
+      <label className="label-name" htmlFor="cardholder-name">CARDHOLDER NAME</label>
+      <div className="input-container">
+        <input onChange={handleName} value={props.cardName} type="text" placeholder="e.g. Jane Appleseed" name="cardholder-name" id="cardholder-name" />
+      </div>
     </div>
   )
 }
