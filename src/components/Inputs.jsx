6b25a8187530e92dbc622e7081a721ca358d9451
Ziad Rima <ziadrima49@gmail.com>
2025-03-13 13:25:50 +0100

    finish inputs component

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,8 +1,29 @@
 import CardName from "./CardName"
-const Inputs = () => {
+import CardNumber from "./CardNumber"
+import ExpDate from "./ExpDate"
+const Inputs = (props) => {
   return (
     <div className='inputs-container'>
-      <CardName />
+      <CardName 
+        cardName={props.cardName}
+        setCardName={props.setCardName}
+      />
+      <CardNumber 
+        cardNumber={props.cardNumber} 
+        setCardNumber={props.setCardNumber}
+        errors={props.errors}
+        setErrors={props.setErrors}
+      />
+      <ExpDate 
+        month={props.month}
+        setMonth={props.setMonth}
+        year={props.year}
+        setYear={props.setYear}
+        cvc={props.cvc}
+        setCvc={props.setCvc}
+        errors={props.errors}
+        setErrors={props.setErrors}
+      />
     </div>
   )
 }
