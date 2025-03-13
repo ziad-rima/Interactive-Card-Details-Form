6b978a61f38045ccefd5f2c7d20d3a1f1e095bca
Ziad Rima <ziadrima49@gmail.com>
2025-03-13 13:25:35 +0100

    pass down props to display data

--- src/components/FrontCard.jsx
+++ src/components/FrontCard.jsx
@@ -1,15 +1,15 @@
 import CardLogo from "./CardLogo"
-const FrontCard = () => {
+const FrontCard = (props) => {
   return (
     <div className='front-card-container space-grotesk'>
       <img className="front-card" src="/images/bg-card-front.png" alt="front card image" />
       <div className="front-card-info">
         <CardLogo />
         <div className="card-owner-info">
-            <p className="card-number">0000 0000 0000 0000</p>
+            <p className="card-number">{props.valid ? props.cardNumber : "0000 0000 0000 0000"}</p>
             <div className="name-and-date">
-                <p className="card-owner">JANE APPLESEED</p>
-                <p className="date">00/00</p>
+                <p className="card-owner">{props.valid ? props.cardName : "JANE APPLESEED"}</p>
+                <p className="date">{props.valid ? `${props.month}/${props.year}` : "00/00"}</p>
             </div>
         </div>
       </div>
